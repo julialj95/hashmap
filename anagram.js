1a7bd30a2d0a244db781c249013c6a7671ee9d9c
Julia_Johnson <73026110+julialj95@users.noreply.github.com>
2021-04-26 22:51:48 -0500

    Create anagram.js

---
+++ anagram.js
@@ -0,0 +1,20 @@
+const groupAnagrams = (strArr) => {
+  const anagramMap = new Map()
+  strArr.forEach((word) => {
+    let sorted = alphabetize(word)
+    if(anagramMap.has(sorted)) {
+      anagramMap.get(sorted).push(word)
+    }
+    else {
+      anagramMap.set(sorted, [word])
+    }
+  }) 
+  return [...anagramMap.values()]
+}
+
+const alphabetize = word => {
+  let alphebtized = word.split('').sort().join('')
+  return alphebtized
+}
+
+console.log(groupAnagrams(['east', 'cars', 'acre', 'arcs', 'teas', 'eats', 'race']))
